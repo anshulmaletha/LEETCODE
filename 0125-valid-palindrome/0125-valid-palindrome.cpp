6bab98e707b8b0f2ab50c6ba4f029e8b8e06354d
anshulmaletha <anshul.maletha@gmail.com>
2026-08-27 13:31:04 +0530

    Time: 0 ms (100%), Space: 9.7 MB (94.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s1 = "";
-
-        for(int i = 0; i < s.size(); i++){
-            if(isalnum(s[i]))
-                s1 += tolower(s[i]);
-        }
 
         int left = 0;
-        int right = s1.size() - 1;
+        int right = s.size() - 1;
 
-        while(left < right){
-            if(s1[left] != s1[right])
+        while(left < right) {
+
+            while(left < right && !isalnum(s[left]))
+                left++;
+
+            while(left < right && !isalnum(s[right]))
+                right--;
+
+            if(tolower(s[left]) != tolower(s[right]))
                 return false;
 
             left++;
@@ -22,3 +23,24 @@ public:
         return true;
     }
 };
+
+        // string s1 = "";
+
+        // for(int i = 0; i < s.size(); i++){
+        //     if(isalnum(s[i])){
+        //         s1 += tolower(s[i]);
+        //     }
+        // }
+
+        // int left = 0;
+        // int right = s1.size() - 1;
+
+        // while(left < right){
+        //     if(s1[left] != s1[right])
+        //         return false;
+
+        //     left++;
+        //     right--;
+        // }
+
+        // return true;
